f014a714cd01275adbd5163176dc8f87a9c86656
mdfirdaus700 <mdfirdausansari700@gmail.com>
2025-08-25 10:57:41 +0000

    initial commit

---
+++ shell.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#================================
+#
+#Author: Md Firdaus
+#Date: 25/08/2025
+#Description: here we will learn concept of shell scripts.
+#
+echo "A shell scripts is text file containing a series of commands written for the linux/unix shell to execute.Instead of typing command one by one. you put them in .sh file and run them"
+
+echo "#!/bin/bash :- Shebang, tells system to use bash shell!"
+
+echo ".sh extention :- convension for shell scripts!"
+
+echo "echo :- print text!"
+
+echo "why use shell scripts:-
+    1. Automate repetitive tasks
+    2. Manage servers (Start/stop services,monitoring,backups)
+    3. Deployment & CI/CD pipelines
+    4. File management (search,move,compress,backup,delete
+    5. Monitoring (disk usage,CPU,logs,etc.)!"
+#
